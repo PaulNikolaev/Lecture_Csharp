620537a50bfeb3493186b15fd95fbe5e76fbe887
Poul Nikolaev <panikolaev7@gmail.com>
2024-02-21 09:16:29 +0300

    add Lesson_2: Task_3

---
+++ Lesson_2/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] array = { 2, 5, 6, 3, 7, 9, 3, 1, 18, 21 };
+int i = 0;
+
+while (i < array.Length) 
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
