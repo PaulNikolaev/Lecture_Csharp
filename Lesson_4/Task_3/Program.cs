ee035261896b6fa0aaeee15d1db64f4d6ce93379
Poul Nikolaev <panikolaev7@gmail.com>
2024-02-28 12:01:34 +0300

    add Lesson_4: Task_3

---
+++ Lesson_4/Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
