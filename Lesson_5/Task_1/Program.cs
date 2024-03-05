2571db57ff1d3bb3d1585fe6beddd0e3aa9deaef
Poul Nikolaev <panikolaev7@gmail.com>
2024-03-05 12:27:45 +0300

    add Lesson_5: Task_1

---
+++ Lesson_5/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычислить факториал от натурального числа N
+
+Console.Clear();
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * Fact(n - 1);
+}
+
+Console.WriteLine(Fact(5));
